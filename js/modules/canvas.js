c137c1fade763d88b1c9cd27b4b0b31cc5992015
Florian Schildwächter <fs@schumacher-visuell.de>
2018-03-29 00:03:17 +0200

    paint starting point on enterkey press

--- js/modules/canvas.js
+++ js/modules/canvas.js
@@ -2,6 +2,10 @@ $( document ).ready(function() {
     var canvas = document.getElementById('canvas');
     var ctx = canvas.getContext('2d');
 
-    ctx.fillStyle = 'rgb(200, 0, 0)';
-    ctx.fillRect(100, 50, 30, 100);
+    $(document).keypress(function(key) {
+        if(key.which == 13) {
+          ctx.fillStyle = 'rgb(200, 0, 0)';
+          ctx.fillRect(0, 0, 5, 5);
+        }
+    })
 });
