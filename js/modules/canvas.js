fa46bd9bc759aacb0c593d8da606341c3a4460d1
Florian Schildwächter <fs@schumacher-visuell.de>
2018-03-29 09:45:36 +0200

    created loop for game frames

--- js/modules/canvas.js
+++ js/modules/canvas.js
@@ -1,11 +1,36 @@
-$(document).ready(function() {
-    var canvas = $('#canvas')[0];
-    var ctx = canvas.getContext('2d');
+var JS_SNAKE = {};
 
-    $(document).keypress(function(key) {
-        if(key.which == 13) {
-          ctx.fillStyle = 'rgb(200, 0, 0)';
-          ctx.fillRect(0, 0, 5, 5);
-        }
-    })
+JS_SNAKE.game = (function () {
+  var ctx;
+  var xPosition = 0;
+  var yPosition = 0;
+  var frameLength = 500;
+
+  function init() {
+    $('body').append('<canvas id="jsSnk">');
+    var canvas = $('#jsSnk');
+    canvas.attr('width', 100);
+    canvas.attr('height', 100);
+    var canvas = canvas[0];
+    ctx = canvas.getContext('2d');
+    gameLoop();
+  }
+
+  function gameLoop() {
+    xPosition += 2;
+    yPosition += 4;
+    ctx.clearRect(0, 0, 100, 100);
+    ctx.fillStyle = '#fe57a1';
+    ctx.fillRect(xPosition, yPosition, 30, 50);
+    setTimeout(gameLoop, frameLength);
+  }
+
+  return {
+    init: init
+  };
+})();
+
+
+$(document).ready(function () {
+  JS_SNAKE.game.init();
 });
